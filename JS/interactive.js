72402c7df10533be7c0bbe10e923139bc4cc9d62
Nick077mp <mn777aragorn@gmail.com>
2023-09-14 18:41:17 -0500

    Getting Documents. First Time

---
+++ JS/interactive.js
@@ -0,0 +1,13 @@
+
+
+window.onload = function () {
+    const navToggle = document.querySelector(".nav-toggle")
+    const navMenu = document.querySelector(".nav_menu")
+
+    navToggle.addEventListener('click', () => {
+
+        navMenu.classList.toggle("nav-menu_visible");
+
+
+    });
+}
